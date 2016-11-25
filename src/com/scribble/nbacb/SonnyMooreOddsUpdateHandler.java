d7944d50c1b528860e5167638e3f701343d16c0a
Chandramouli S <cmouli_84@yahoo.co.in>
2016-11-24 22:09:13 -0800

    Partial updates for NFL and NCAAB

--- src/com/scribble/nbacb/SonnyMooreOddsUpdateHandler.java
+++ src/com/scribble/nbacb/SonnyMooreOddsUpdateHandler.java
@@ -1,21 +1,10 @@
 package com.scribble.nbacb;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.ItemCollection;
-import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.scribble.nbacb.models.SonnyMoorePrediction;
 import com.scribble.nbacb.service.CollegeBasketBallOddsService;
 
 public class SonnyMooreOddsUpdateHandler implements RequestHandler<Object, Boolean> {
@@ -25,33 +14,16 @@ public class SonnyMooreOddsUpdateHandler implements RequestHandler<Object, Boole
         context.getLogger().log("Input: " + input);
         
         DynamoDB dynamoDb = new DynamoDB(Regions.US_WEST_2);
-
         Table events = dynamoDb.getTable("NflEvents");
         
         CollegeBasketBallOddsService service = new CollegeBasketBallOddsService();
 
-		try {
-			List<SonnyMoorePrediction> sonnyMoorePredictions = service.getSonnyMooreRankings(events);
-
-	        for (SonnyMoorePrediction prediction: sonnyMoorePredictions)
-	        {
-	            Item currentEvent = new Item();
-	            currentEvent.withInt("EventId", prediction.getEventId());
-	            currentEvent.withString("HomeTeamName", prediction.getHomeTeamName());
-	            currentEvent.withString("AwayTeamName", prediction.getAwayTeamName());
-	            currentEvent.withDouble("HomeTeamRanking", prediction.getHomeTeamRanking());
-	            currentEvent.withDouble("AwayTeamRanking", prediction.getAwayTeamRanking());
-	            currentEvent.withString("EventDate", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(prediction.getGameDate()));
-	            
-	        	events.putItem(currentEvent);
-	        }
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        try {
+    		service.updateNflSonnyMooreRankings(events);
+        }
+        catch (Exception ex) {
+        	System.out.println(ex.toString());
+        }
         
         // TODO: implement your handler
         return true;
--- src/com/scribble/nbacb/service/CollegeBasketBallOddsService.java
+++ src/com/scribble/nbacb/service/CollegeBasketBallOddsService.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import com.amazonaws.services.dynamodbv2.document.AttributeUpdate;
 import com.amazonaws.services.dynamodbv2.document.Item;
@@ -24,14 +22,17 @@ public class CollegeBasketBallOddsService {
 
 	private CollegeBasketBallOddsRepository nbacbRepository = new CollegeBasketBallOddsRepository();
 
-	public List<SonnyMoorePrediction> getSonnyMooreRankings(Table eventsTable) 
+	public void updateNflSonnyMooreRankings(Table eventsTable) 
 			throws IOException, ParseException
 	{
-		List<SonnyMoorePrediction> sonnyMoorePredictions = new ArrayList<>();
-		
 		List<PowerRanking> sonnyMoorePowerRankings = nbacbRepository.getSonnyMoorePowerRaking();
 		List<Event> events = nbacbRepository.getCurrentEvents();
 
+		updateSonnyMooreRanking(eventsTable, sonnyMoorePowerRankings, events);		
+	}
+
+	private void updateSonnyMooreRanking(Table eventsTable, List<PowerRanking> sonnyMoorePowerRankings, List<Event> events)
+			throws ParseException {
 		Calendar toDateCalendar = Calendar.getInstance();
 		Date currentTime = toDateCalendar.getTime();
 		toDateCalendar.add(Calendar.HOUR, 1);
@@ -71,8 +72,6 @@ public class CollegeBasketBallOddsService {
 				updateEvent(eventsTable, event);
 			}
 		}
-		
-		return sonnyMoorePredictions;
 	}
 	
 	private void addEvent(Table events, SonnyMoorePrediction prediction)
@@ -159,90 +158,4 @@ public class CollegeBasketBallOddsService {
 		
 		return null;
 	}
-	
-/*	private PowerRanking TryFindMatch(String teamName, List<PowerRanking> matches, String pattern)
-	{
-		ArrayList<PowerRanking> secondMatch = new ArrayList<>();
-		if (pattern.equals("HC"))
-		{
-			String matchingTeamName = "";
-			switch (teamName)
-			{
-				case "UNLV":
-					matchingTeamName = "NEVADA LAS VEGAS";
-					break;
-				case "TEXAS CHRISTIAN":
-					matchingTeamName = "TCU";
-					break;
-				case "INDIANA   PURDUE":
-					matchingTeamName = "IUPUI";
-					break;
-				case "SOUTHERN CALIFORNIA":
-					matchingTeamName = "SOUTHERN CAL";
-					break;
-				case "LOUISIANA STATE":
-					matchingTeamName = "LSU";
-					break;
-				case "LOYOLA CHICAGO":
-					matchingTeamName = "LOYOLA ILLINOIS";
-					break;
-				case "BRIGHAM YOUNG":
-					matchingTeamName = "BYU";
-					break;
-				case "CAL POLY SLO":
-					matchingTeamName = "CAL POLY";
-					break;
-				case "SOUTHERN METHODIST":
-					matchingTeamName = "SMU";
-					break;
-				case "CHARLESTON":
-					matchingTeamName = "COLLEGE OF CHARLESTON";
-					break;
-			}
-			
-			for (PowerRanking team : matches)
-			{
-				if (team.getTeamName().equals(matchingTeamName))
-				{
-					return team;
-				}
-			}
-			return null;
-		}
-		if (pattern.equals("FW"))
-		{
-			for (PowerRanking matchName: matches)
-			{
-				if (matchName.getTeamName().split(" ")[0].equals(teamName.split(" ")[0]))
-				{
-					secondMatch.add(matchName);
-				}
-			}
-			
-			if (secondMatch.size() == 1)
-			{
-				return secondMatch.get(0);
-			}
-			return TryFindMatch(teamName, matches, "LW");
-		}
-		if (pattern.equals("LW"))
-		{
-			for (PowerRanking matchName: matches)
-			{
-				String[] matchArray = matchName.getTeamName().split(" ");
-				String[] teamNameArray = teamName.split(" ");
-				if (matchArray[matchArray.length - 1].equals(teamNameArray[teamNameArray.length - 1]))
-				{
-					secondMatch.add(matchName);
-				}
-			}
-			
-			if (secondMatch.size() == 1)
-			{
-				return secondMatch.get(0);
-			}
-		}
-		
-		return null;
-	}*/
 }
